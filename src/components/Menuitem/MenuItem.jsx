84f2c3b38ddf381ca856c82dc0a7e745bff08325
Izan Lopez <e.lopezruiz123@gmail.com>
2023-08-29 22:06:40 +0100

    Special menu added

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import './MenuItem.css';
+import "./MenuItem.css";
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({ title, desc, price }) => (
+  <div className="app__menuitem">
+    <div className="app__menuitem-top">
+      <div className="app__menuitem-top_name">
+        <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+          {title}
+        </p>
+      </div>
+      <div className="app__menuitem-top_bar"></div>
+      <p className="app__menuitem-top_price">{price}</p>
+    </div>
+    <div className="app__menuitem-bottom">
+      <p>{desc}</p>
+    </div>
   </div>
 );
 
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React from "react";
 
-import './SpecialMenu.css';
+import "./SpecialMenu.css";
+import { images, data } from "../../constants";
+import { SubHeading, MenuItem } from "../../components";
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className="app__specialmenu section__padding flex__center app__bg">
+    <div className="app__specialmenu-header">
+      <SubHeading title="Menu that Fits Your Palatte" />
+      <h1 className="headtext__cormorant">Today's Specials</h1>
+    </div>
+    <div className="app__specialmenu-content">
+      <div className="app__specialmenu-content_winebeer">
+        <h1 className="p__cormorant" style={{ marginBottom: "28px" }}>
+          Wine & Beer
+        </h1>
+        <div>
+          {data.wines.map((wine, index) => (
+            <MenuItem title={wine.title} desc={wine.tags} price={wine.price} />
+          ))}
+        </div>
+      </div>
+      <img src={images.menu} />
+      <div className="app__specialmenu-content_cocktails">
+        <h1 className="p__cormorant" style={{ marginBottom: "28px" }}>
+          Cocktails
+        </h1>
+        <div>
+          {data.cocktails.map((cocktail, index) => (
+            <MenuItem
+              title={cocktail.title}
+              desc={cocktail.tags}
+              price={cocktail.price}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+    <div className="app__specialmenu-button">
+      <button type="button" className="custom__button">
+        View More
+      </button>
+    </div>
   </div>
 );
 
